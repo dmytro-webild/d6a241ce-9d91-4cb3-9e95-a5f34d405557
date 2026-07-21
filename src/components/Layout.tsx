2afc4e9e53fdf71b475383fc222c71ac84f9dc61
bender <kudin.dima@gmail.com>
2026-07-21 18:08:51 +0000

    Update src/components/Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -34,7 +34,7 @@ export default function Layout() {
       brand="Powery & Asociados"
       columns={[
         {
-          title: "Company",
+          title: "Eco‑tourism and hospitality",
           items: [
             {
               label: "About Us",
